bf7cf5b6c563877835acf550ab0ac9e716812e94
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-20 11:54:28 +0530

    .

--- hello.go
+++ hello.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"hello-service/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "Hello microservice health check!")
-	})
-
+	r.GET("/ping", handlers.Ping)
 	r.GET("/hye/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		if name == "" {
